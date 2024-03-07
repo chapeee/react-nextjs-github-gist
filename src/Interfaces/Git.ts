f28c56282dc4e706748ce93c811a0aba08a083b9
GrapDevs.com <rajneesh@grapdevs.com>
2024-03-07 22:33:40 +0530

    making major changes to the code

---
+++ src/Interfaces/Git.ts
@@ -0,0 +1,16 @@
+export interface CreateIframeProps {
+    iframeRef: any;
+    width: any;
+    height: any;
+    file: any;
+    id: any;
+}
+
+export interface GistProps {
+    id: string;
+    file?: string;
+    width?: string;
+    height?: string;
+    getProfile?: string;
+    userName?: string
+}
